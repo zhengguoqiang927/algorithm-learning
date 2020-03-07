265cfce9958ea0635b048897d61d6c88dc3f3989
zhengguoqiang927 <zhengguoqiang927@gmail.com>
2020-03-07 11:10:52 +0800

    add stack

---
+++ src/main/java/com/zhengguoqiang/stack/StackImpl.java
@@ -0,0 +1,9 @@
+package com.zhengguoqiang.stack;
+
+import java.util.Queue;
+import java.util.concurrent.DelayQueue;
+
+public class StackImpl {
+    private static Queue queue = new DelayQueue();
+
+}
