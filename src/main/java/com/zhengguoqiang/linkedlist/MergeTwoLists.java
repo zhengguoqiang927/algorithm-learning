cb981f33be8ec44691b191ca6bb0bd26be417e45
zhengguoqiang <zhengguoqiang927@gmail.com>
2020-06-15 11:23:04 +0800

    add algorithm

--- src/main/java/com/zhengguoqiang/linkedlist/MergeTwoLists.java
+++ src/main/java/com/zhengguoqiang/linkedlist/MergeTwoLists.java
@@ -9,5 +9,47 @@ public class MergeTwoLists {
         }
     }
 
+    public static ListNode merge(ListNode p1,ListNode p2){
+        if (p1 == null) return p2;
+        ListNode head = p1;
+        while (true){
+            if (p1 == null){
+                head.next = p2;
+                break;
+            }
+            if (p2 == null){
+                head.next = p1;
+                break;
+            }
+            if (p1.val < p2.val){
+                head = p1;
+                p1 = p1.next;
+            }else{
 
+            }
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode ln1 = new ListNode(1);
+        ListNode ln2 = new ListNode(3);
+        ListNode ln3 = new ListNode(5);
+        ListNode ln4 = new ListNode(7);
+        ln1.next = ln2;
+        ln2.next = ln3;
+        ln3.next = ln4;
+        ln4.next = null;
+
+        ListNode rn1 = new ListNode(2);
+        ListNode rn2 = new ListNode(4);
+        ListNode rn3 = new ListNode(6);
+        ListNode rn4 = new ListNode(8);
+        rn1.next = rn2;
+        rn2.next = rn3;
+        rn3.next = rn4;
+        rn4.next = null;
+
+
+    }
 }
--- src/test/java/com/zhengguoqiang/AppTest.java
+++ src/test/java/com/zhengguoqiang/AppTest.java
@@ -4,6 +4,9 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Unit test for simple App.
  */
@@ -35,4 +38,47 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+    public static int binarySearch(int[] a,int value){
+        int low = 0;
+        int high = a.length - 1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if (value == a[mid]){
+                return mid;
+            }else if (value > a[mid]){
+                low = mid + 1;
+            }else {
+                high = mid -1;
+            }
+        }
+        return -1;
+    }
+
+    public static int binarySearchByRecusive(int[] a,int lo,int hi,int value){
+        if (lo > hi) return -1;
+        int mid = lo + (hi - lo)/2;
+        if (value == a[mid]){
+            return mid;
+        }else if (value > a[mid]){
+            return binarySearchByRecusive(a,mid + 1,hi,value);
+        }else {
+            return binarySearchByRecusive(a,lo,mid - 1,value);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,5,6,7,8};
+        int i = binarySearch(a,3);
+        System.out.println("index I :" + i);
+        int j = binarySearchByRecusive(a,0,a.length-1,3);
+        System.out.println("index J :" + j);
+
+        String s = "flsjdsadfljs\r\nsfjsldkfj";
+        String s1 = s.replaceAll("[\\r\\n]", "\n");
+//        String s2 = s1.replaceAll("^\\n$","00");
+        System.out.println(s1);
+    }
+
+
 }
