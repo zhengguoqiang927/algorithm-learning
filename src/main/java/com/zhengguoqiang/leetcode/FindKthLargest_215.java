3361983ae69a200417b184f68786921657f88ea5
zhengguoqiang <zhengguoqiang927@gmail.com>
2020-12-08 15:37:57 +0800

    add leetcode 215

---
+++ src/main/java/com/zhengguoqiang/leetcode/FindKthLargest_215.java
@@ -0,0 +1,71 @@
+package com.zhengguoqiang.leetcode;
+
+import java.util.Random;
+
+/**
+ * 215. 数组中的第K个最大元素
+ * 在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [3,2,1,5,6,4] 和 k = 2
+ * 输出: 5
+ * 示例 2:
+ * <p>
+ * 输入: [3,2,3,1,2,4,5,5,6] 和 k = 4
+ * 输出: 4
+ * 说明:
+ * <p>
+ * 你可以假设 k 总是有效的，且 1 ≤ k ≤ 数组的长度。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/kth-largest-element-in-an-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class FindKthLargest_215 {
+    Random random = new Random();
+
+    public int findKthLargest(int[] nums, int k) {
+        return quickSelect(nums, 0, nums.length - 1, nums.length - k);
+    }
+
+    private int quickSelect(int[] nums, int l, int r, int index) {
+        int q = randomPartition(nums, l, r);
+        if (q == index) {
+            return nums[q];
+        } else {
+            return q < index ? quickSelect(nums, q + 1, r, index) : quickSelect(nums, l, q - 1, index);
+        }
+    }
+
+    private int randomPartition(int[] nums, int l, int r) {
+        int i = random.nextInt(r - l + 1) + l;
+        swap(nums, i, r);
+        return partition(nums, l, r);
+    }
+
+    private int partition(int[] nums, int l, int r) {
+        int x = nums[r], i = l - 1;
+        for (int j = l; j < r; j++) {
+            if (nums[j] <= x) {
+                swap(nums, ++i, j);
+            }
+        }
+        swap(nums, i + 1, r);
+        return i + 1;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 1, 5, 6, 4};
+        FindKthLargest_215 findK = new FindKthLargest_215();
+        int kthLargest = findK.findKthLargest(nums, 5);
+        System.out.println("The k largest num is " + kthLargest);
+    }
+
+}
